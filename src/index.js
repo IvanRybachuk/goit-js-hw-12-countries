5e8aa6b0a622c0b2e5b0425773be93f4f2118dfe
IvanRybachuk <68922258+IvanRybachuk@users.noreply.github.com>
2020-11-02 20:55:01 +0300

    1
    1

--- src/index.js
+++ src/index.js
@@ -10,18 +10,18 @@ const serch = document.querySelector('.input-country');
 const show = function (data) {
   if (data.status === 404) {
     output.innerHTML = 'По вашему запросу ничего не найдено';
-    return
+    return;
   } else if (data.length > 10) {
     output.innerHTML = 'привышен лимит';
-    return
+    return;
   }
   if (data.length > 1 && data.length < 10) {
     output.innerHTML = data.reduce(
-      (acc, el) => acc + `<ul><li>${el.name}</li></ul>`,
+      (acc, el) => acc + `<ul><li class="cauntrys">${el.name}</li></ul>`,
       '',
     );
   } else {
-      output.innerHTML = template(data[0])
+    output.innerHTML = template(data[0]);
   }
 };
 console.log(serch);
@@ -33,4 +33,20 @@ const onInput = function () {
     show(res);
   });
 };
+if (localStorage.state) {
+  serch.value = localStorage.state;
+  onInput();
+}
 serch.addEventListener('input', debounce(onInput, 500));
+
+window.addEventListener('beforeunload', () => {
+  localStorage.setItem('state', serch.value);
+});
+
+output.addEventListener('click', e => {
+  if (e.target.classList.contains('cauntrys')) {
+    serch.value = e.target.textContent;
+
+    onInput();
+  }
+});
